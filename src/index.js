d8814513a75a3e021ccab22c5cb5f5e2ec0e34e4
syamsundarsai <msyamsundarsai@gmail.com>
2023-08-03 12:50:50 +0530

    setup logger

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 
 import app from "./app.js"
+import logger from "./configs/logger.config.js"
 //dotenv config()
 const PORT = process.env.PORT || 8000
 console.log(process.env.NODE_ENV)
 app.listen(PORT, () => {
-    console.log(`server listening the port Number ${PORT}`)
+    logger.info (`server listening the port Number ${PORT}`)
 })
