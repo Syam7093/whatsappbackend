f7f3e8a708bf5a26a755e439d738bbedf56fcbda
syamsundarsai <msyamsundarsai@gmail.com>
2023-08-03 12:35:13 +0530

    setup all the modules and cross-env

--- src/app.js
+++ src/app.js
@@ -1,10 +1,30 @@
-const express=require("express")
-
-
+import express from "express"
+import dotenv from "dotenv"
+import morgan from "morgan"
+import helmet from "helmet"
+import mongoSanitize from "express-mongo-sanitize"
+import cookieParser from "cookie-parser"
+import compression from "compression"
+import fileUpload from "express-fileupload"
+import cors from "cors"
+dotenv.config()
 const app = express()
-
-app.get("/", (req, res) => {
-    res.send("welcome to whats app")
+app.use(express.json())
+app.use(morgan("dev"))
+app.use(express.urlencoded({ extended: true }))
+app.use(helmet())
+app.use(mongoSanitize())
+app.use(cookieParser())
+app.use(compression())
+app.use(fileUpload({
+    useTempFiles:true 
+}))
+// app.use(cors({
+//     origin:"http://localhost:3000"
+// }))
+// app.use(cors())
+app.post("/test", (req, res) => {
+    res.send(req.body)
 })
 
 export default app
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-const dotenv=require("dotenv").config()
-import app from "./app"
+
+import app from "./app.js"
 //dotenv config()
 const PORT = process.env.PORT || 8000
+console.log(process.env.NODE_ENV)
 app.listen(PORT, () => {
     console.log(`server listening the port Number ${PORT}`)
 })
